5ea5cedd0ef66fc020beef22a81c364dd6f3a09d
Renan Jansen <renan.jansen@dbseller.com.br>
2023-05-27 14:31:02 -0300

    Update : Repopulando formulário

--- resources/views/site/layouts/_components/form_contato.blade.php
+++ resources/views/site/layouts/_components/form_contato.blade.php
@@ -1,11 +1,11 @@
 ﻿
 <form action={{ route('site.contato') }} method="post">
     @csrf
-    <input type="text" placeholder="Nome" name='nome' class={{ $classe }}>
+    <input type="text" value="{{ old('nome') }}" placeholder="Nome" name='nome' class={{ $classe }}>
     <br>
-    <input type="text" placeholder="Telefone" name="telefone" class={{ $classe }}>
+    <input type="text" value="{{ old('telefone') }}" placeholder="Telefone" name="telefone" class={{ $classe }}>
     <br>
-    <input type="text" placeholder="E-mail" name="email" class={{ $classe }}>
+    <input type="text" value="{{ old('email') }}" placeholder="E-mail" name="email" class={{ $classe }}>
     <br>
     <select class={{ $classe }} name="motivo_contato">
         <option value="1">Qual o motivo do contato?</option>
@@ -14,7 +14,11 @@
         <option value="4">Reclamação</option>
     </select>
     <br>
-    <textarea class={{ $classe }} name="mensagem" placeholder="Preencha aqui a sua mensagem"></textarea>
+    <textarea class={{ $classe }} name="mensagem" placeholder="Preencha aqui a sua mensagem">
+        @if ( old('mensagem')  != '')
+            {{ old('mensagem') }}
+        @endif
+    </textarea>
     <br>
     <button type="submit" class={{ $classe }}>ENVIAR</button>
 </form>
